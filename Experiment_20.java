6b1cb4e73e1a1b8326ccd0dca7d4376655a6c0d5
Aditya Kumar Singh <simghaditya21@gmail.com>
2026-02-12 14:41:18 +0530

    Experiment - 2]

---
+++ Experiment_20.java
@@ -0,0 +1,14 @@
+public class Experiment_20 {
+    public static void main(String args[]){
+        try{
+            int a = 10/0;
+        }
+        catch(ArithmeticException e){
+            System.out.println("Cannot divide by zero");
+
+        }
+        finally{
+            System.out.println("Done");
+        }
+    }
+}
